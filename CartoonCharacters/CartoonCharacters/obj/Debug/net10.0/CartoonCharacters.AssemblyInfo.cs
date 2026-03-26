bb4d97513a60d797a5d94fd7233c5ba235521b5e
ali <ali.ucar@student.helb-prigogine.be>
2026-03-26 16:15:30 +0100

    ajout scanner V2

--- CartoonCharacters/CartoonCharacters/obj/Debug/net10.0/CartoonCharacters.AssemblyInfo.cs
+++ CartoonCharacters/CartoonCharacters/obj/Debug/net10.0/CartoonCharacters.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CartoonCharacters")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8b291fa37a76731bd52480b553ea23410556f04")]
 [assembly: System.Reflection.AssemblyProductAttribute("CartoonCharacters")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CartoonCharacters")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
